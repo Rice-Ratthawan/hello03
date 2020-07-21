84081bb84dfd272a0eac22b388e705bda2924857
Ratthawan Sermsuwannasuk <ratthawanser@mathregit.group>
2020-07-21 22:36:36 +0700

    try

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import {User} from './../models/user'
+import { User } from './../models/user'
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
@@ -9,44 +9,45 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 
 export class UsersComponent implements OnInit {
-  
+
+  //in workshop03 users = [];
   users: User[] = []; //Empty Array
   show = true; //show: boolean = true->show, false->not show
   selected = 0;
   submitted = false;
 
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}  
-  
+  constructor(private formBuilder: FormBuilder) { }
+
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      firstName: ['', [Validators.required,Validators.maxLength(20)]],
+      firstName: ['', [Validators.required, Validators.maxLength(20)]],
       lastName: ['', Validators.required],
     })
   }
-  
+
   get f() {
     return this.loginForm.controls;
   }
 
-    saveData(): void {
+  saveData(): void {
     this.submitted = true;
-    if(this.loginForm.invalid){
+    if (this.loginForm.invalid) {
       return;
     }
     //String concat-> this.name=this.firstName+' '+this.lastName; 
     const fullName = `${this.f.firstName.value} ${this.f.lastName.value}`; //String template
     //Append to list/array
     //this.users.push({name: fullName, status: false});
-    const user = new User(fullName,false);
+    const user = new User(fullName, false);
     this.users.push(user);
-    
+
   }
 
   doToggle(index: number) {
     this.users[index].status = !this.users[index].status;
     this.selected = this.users.filter((u) => u.status).length;
   }
-  
-  
+
+
 }
